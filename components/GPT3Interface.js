4b9a873aee15dc1b65cbe161501fcf25820fa69e
Alvin Zhang <alvin.zhang295@gmail.com>
2022-12-06 19:42:01 -0600

    Connect to OpenAI API

--- components/GPT3Interface.js
+++ components/GPT3Interface.js
@@ -1,17 +1,40 @@
 import { useState } from "react";
-import sendGPTRequest from "../pages/api/GPTAPIs";
+const { Configuration, OpenAIApi } = require("openai");
 
 export default function GPTInterface(props) {
   const [textInput, setTextInput] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
   const handleInputChange = (event) => {
     setTextInput(event.target.value);
   };
+  const handleInput = async (event) => {
+    if (!submitted) {
+      let GPTInput = document.getElementById("gptsetup");
+      //   let URL = process.env.NEXT_PUBLIC_GPT_Microservice_URL + "generateSchema";
+      //   console.log(process.env.NEXT_PUBLIC_OPENAI_API_KEY);
+      const configuration = new Configuration({
+        apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+      });
+      const openai = new OpenAIApi(configuration);
+
+      const completion = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: textInput,
+      });
+      let ret = completion.data.choices[0].text;
+      console.log(ret);
+    }
+  };
   return (
-    <form>
-      <input type="text" value={textInput} onChange={handleInputChange} />
-      <button type="submit" onClick={sendGPTRequest}>
-        Submit
-      </button>
-    </form>
+    <div>
+      <input
+        id="gptsetup"
+        type="text"
+        value={textInput}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleInput}>Submit</button>
+    </div>
   );
 }
